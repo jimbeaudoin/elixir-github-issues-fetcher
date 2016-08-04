1f91b0e65e00c1963142e064fe8492596a488b4d
Jimmy Beaudoin <email@jimmy-beaudoin.com>
2016-08-04 18:51:01 -0400

    Add doc to the project

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      escript: escript_config,
      version: "0.1.0",
+     name: "Issues",
+     source_url: "https://github.com/jimbeaudoin/elixir-github-issues-fetcher",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -29,8 +31,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison: "~> 2.2"
+      {:httpoison, "~> 0.9"},
+      {:poison,    "~> 2.2"},
+      {:ex_doc,    "~> 0.12"},
+      {:earmark,   "~> 1.0", override: true}
     ]
   end
 
